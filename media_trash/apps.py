153ef844c96bcfe22aa3d082e878ff826616f2dd
alexsilva <geniofuturo@gmail.com>
2018-09-06 10:52:40 -0300

    App config setup.

---
+++ media_trash/apps.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+from django.apps import AppConfig
+
+
+class MediaTrashConfig(AppConfig):
+    """Media trash config"""
+    name = 'media_trash'
+
+    # verbose_name = u"Media Trash"
