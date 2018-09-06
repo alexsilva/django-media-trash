f8f02c0299c2c8d325affe68bbdb92fd1c55309e
alexsilva <geniofuturo@gmail.com>
2018-09-06 13:30:47 -0300

    Sample listing.

--- media_trash/views.py
+++ media_trash/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from . import settings
+from .base import FileListing
+
 
 class MediaView(View):
 
     def get(self, request, *args, **kwargs):
-        context = {}
+        flisting = FileListing(settings.MEDIA_TRASH_PATH)
+
+        context = {
+            'file': flisting
+        }
         return render(request, 'media-trash/index.html', context=context)
