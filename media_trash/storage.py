d906d64e7b2db61f643d5fca96ce053605c2c152
alexsilva <geniofuturo@gmail.com>
2018-09-06 13:14:47 -0300

    Api to list files.

---
+++ media_trash/storage.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+
+import os
+import shutil
+
+from django.core.files.move import file_move_safe
+from django.core.files import storage
+from django.utils.encoding import smart_text
+
+from .settings import DEFAULT_PERMISSIONS
+
+
+class StorageMixin(object):
+    """
+    Adds some useful methods to the Storage class.
+    """
+
+    def isdir(self, name):
+        """
+        Returns true if name exists and is a directory.
+        """
+        raise NotImplementedError()
+
+    def isfile(self, name):
+        """
+        Returns true if name exists and is a regular file.
+        """
+        raise NotImplementedError()
+
+    def move(self, old_file_name, new_file_name, allow_overwrite=False):
+        """
+        Moves safely a file from one location to another.
+
+        If allow_ovewrite==False and new_file_name exists, raises an exception.
+        """
+        raise NotImplementedError()
+
+    def makedirs(self, name):
+        """
+        Creates all missing directories specified by name. Analogue to os.mkdirs().
+        """
+        raise NotImplementedError()
+
+    def rmtree(self, name):
+        """
+        Deletes a directory and everything it contains. Analogue to shutil.rmtree().
+        """
+        raise NotImplementedError()
+
+    def setpermission(self, name):
+        """
+        Sets file permission
+        """
+        raise NotImplementedError()
+
+
+class FileSystemStorageMixin(StorageMixin):
+
+    def isdir(self, name):
+        return os.path.isdir(self.path(name))
+
+    def isfile(self, name):
+        return os.path.isfile(self.path(name))
+
+    def move(self, old_file_name, new_file_name, allow_overwrite=False):
+        file_move_safe(self.path(old_file_name), self.path(new_file_name), allow_overwrite=True)
+
+    def makedirs(self, name):
+        os.makedirs(self.path(name))
+
+    def rmtree(self, name):
+        shutil.rmtree(self.path(name))
+
+    def setpermission(self, name):
+        from .base import FileObject
+
+        full_path = FileObject(smart_text(name), site=self).path_full
+        os.chmod(full_path, DEFAULT_PERMISSIONS)
+
+
+class FileSystemStorage(storage.FileSystemStorage, FileSystemStorageMixin):
+    """ StorageMixin """
