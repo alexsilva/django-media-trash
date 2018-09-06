c9378dec489dca6991d6d5af95b9785cf347b692
alexsilva <geniofuturo@gmail.com>
2018-09-06 11:04:55 -0300

    Change url.

--- media_trash/urls.py
+++ media_trash/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url("/media-trash", views.MediaView.as_view(), name='media-trash')
+    url("^$", views.MediaView.as_view(), name='media-trash')
 ]
