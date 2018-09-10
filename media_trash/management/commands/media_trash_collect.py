3682beb46780788d4fbe6d38ec1144fde102a01c
alexsilva <geniofuturo@gmail.com>
2018-09-10 11:22:07 -0300

    Safer command.

--- media_trash/management/commands/media_trash_collect.py
+++ media_trash/management/commands/media_trash_collect.py
@@ -1,13 +1,28 @@
 import os
 import shutil
+import traceback
 
 from django.apps import apps
+from django.core.files.move import file_move_safe
 from django.core.management import BaseCommand
 
 from ... import settings
 
 
 class Command(BaseCommand):
+
+    @staticmethod
+    def _path_normalize(path):
+        return os.path.normcase(os.path.abspath(os.path.normpath(path)))
+
+    @classmethod
+    def _is_samefile(cls, src, dst):
+        if hasattr(os.path, 'samefile'):  # Macintosh, Unix.
+            try:
+                return os.path.samefile(src, dst)
+            except OSError:
+                return False
+        return cls._path_normalize(src) == cls._path_normalize(dst)
 
     def handle(self, *args, **options):
         model = apps.get_model(*settings.MEDIA_TRASH_MODEL.split("."))
@@ -16,6 +31,8 @@ class Command(BaseCommand):
 
         if not os.path.isdir(settings.MEDIA_TRASH_PATH):
             os.makedirs(settings.MEDIA_TRASH_PATH)
+
+        recover_dir = settings.MEDIA_TRASH_RECOVER_DIR
 
         for media in objs:
             if not media.exists:
@@ -30,12 +47,18 @@ class Command(BaseCommand):
             if os.path.isfile(src) and not os.path.isdir(dstdir):
                 os.makedirs(dstdir)
 
-            shutil.copy2(src, dst)
+            try:
+                file_move_safe(src, dst, allow_overwrite=True)
+            except OSError:
+                # Avoid hide the error.
+                print traceback.format_exc()
+                continue
 
-            if os.path.exists(dst):
-                os.remove(src)
+            media.delete()
 
-            if len(os.listdir(dstdir)) == 0:
-                os.remove(srcdir)
-
-        objs.delete()
+            if not self._is_samefile(srcdir, recover_dir) and \
+                    not os.listdir(srcdir):
+                try:
+                    shutil.rmtree(srcdir)
+                except OSError:
+                    pass
