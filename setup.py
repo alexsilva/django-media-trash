a200ab5c7587c2ab8ddf391da744d173ee3d5430
alex <geniofuturo@gmail.com>
2020-03-19 10:51:26 -0300

    v2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-media-trash',
-    version='1.0',
+    version='2.0',
     packages=['media_trash',
               'media_trash.management',
               'media_trash.management.commands',
